7d64306d9867e7454674f5b309bec416c3f72095
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2023-03-31 14:22:43 -0400

    COMP: Ensure IFX libraries are also installed alongside the IDTF library
    List of U3D changes:
    
    $ git shortlog 562d1bf70..a2dc4bf9e --no-merges
    Jean-Christophe Fillion-Robin (3):
          COMP: Update CMake install rules to support component-based installation
          COMP: Introduce build options allowing to exclude Samples and IDTFConverter
          COMP: Support setting custom install destination when U3D_SHARED is OF

--- SuperBuild/External_U3D.cmake
+++ SuperBuild/External_U3D.cmake
@@ -29,15 +29,24 @@ if(NOT DEFINED ${proj}_INCLUDE_DIR OR NOT DEFINED ${proj}_IDTF_LIBRARY)
   set(EP_BINARY_DIR ${CMAKE_BINARY_DIR}/${proj}-build)
   set(EP_INSTALL_DIR ${CMAKE_BINARY_DIR}/${proj}-install)
 
+  set(_install_libdir "${CMAKE_INSTALL_LIBDIR}/vtkmodules")
+
   ExternalProject_add(U3D
     GIT_REPOSITORY https://github.com/ClinicalGraphics/u3d.git
-    GIT_TAG 562d1bf70d0d687ac57658f05c4e535819a87d96
+    GIT_TAG a2dc4bf9ead5400939f698d5834b7c5d43dbf42a
     SOURCE_DIR ${EP_SOURCE_DIR}
     BINARY_DIR ${EP_BINARY_DIR}
     INSTALL_DIR ${EP_INSTALL_DIR}
     CMAKE_CACHE_ARGS
+      # Options
       -DU3D_SHARED:BOOL=OFF
+      -DU3D_BUILD_IDTFConverter:BOOL=OFF
+      -DU3D_BUILD_SAMPLES:BOOL=OFF
+      # Install directories
       -DCMAKE_INSTALL_PREFIX:PATH=${EP_INSTALL_DIR}
+      -DBIN_DESTINATION:STRING=${_install_libdir}
+      -DLIB_DESTINATION:STRING=${_install_libdir}
+      -DPLUGIN_DESTINATION:STRING=${_install_libdir}
       ${EXTERNAL_PROJECT_OPTIONAL_CMAKE_CACHE_ARGS}
     USES_TERMINAL_DOWNLOAD 1
     USES_TERMINAL_UPDATE 1
@@ -50,11 +59,14 @@ if(NOT DEFINED ${proj}_INCLUDE_DIR OR NOT DEFINED ${proj}_IDTF_LIBRARY)
 
   if(WIN32)
     # library type hardcoded to "static" in U3D build-system
-    set(${proj}_IDTF_LIBRARY ${EP_INSTALL_DIR}/u3d/IDTF.lib)
+    set(${proj}_IDTF_LIBRARY ${EP_INSTALL_DIR}/${_install_libdir}/IDTF.lib)
   else()
     # library type hardcoded to "shared" in U3D build-system
-    set(${proj}_IDTF_LIBRARY ${EP_INSTALL_DIR}/u3d/${CMAKE_SHARED_LIBRARY_PREFIX}IDTF${CMAKE_SHARED_LIBRARY_SUFFIX})
+    set(${proj}_IDTF_LIBRARY ${EP_INSTALL_DIR}/${_install_libdir}/${CMAKE_SHARED_LIBRARY_PREFIX}IDTF${CMAKE_SHARED_LIBRARY_SUFFIX})
   endif()
+
+  set(${proj}_BINARY_DIR ${EP_BINARY_DIR})
+  ExternalProject_Message(${proj} "${proj}_BINARY_DIR:${${proj}_BINARY_DIR}")
 
 endif()
 
@@ -62,6 +74,7 @@ mark_as_superbuild(
   VARS
     ${proj}_INCLUDE_DIR:PATH
     ${proj}_IDTF_LIBRARY:FILEPATH
+    ${proj}_BINARY_DIR:PATH
   )
 
 ExternalProject_Message(${proj} "${proj}_INCLUDE_DIR:${${proj}_INCLUDE_DIR}")
